443f18f8c00801524829c8ac0836348a0debb8e7
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-31 21:57:18 +0800

    Fourth Commit

--- main.cpp
+++ main.cpp
@@ -10,15 +10,36 @@ struct Node {
 
 };
 
-void push(Node** head, int value) {
-    Node *p = (Node*) malloc(sizeof(Node));
-    p->value = value;
-    p->next = *head;
-    *head = p;
+bool isEmpty(Node* top) {
+    return top == NULL;
 }
 
-void pop(Node** head) {
+void push(Node** top, int value) {
+    Node *p = (Node*) malloc(sizeof(Node));
+    p->value = value;
+    p->next = *top;
+    *top = p;
+}
 
+int pop(Node** top) {
+    if (isEmpty(*top)) {
+        return -1;
+    }
+
+    Node *p = *top;
+    int value = (*top)->value;
+    *top = (*top)->next;
+    free(p);
+
+    return value;
+}
+
+int peek(Node* top) {
+    if (isEmpty(top)) {
+        return -1;
+    }
+    int value = top->value;
+    return value;
 }
 
 int main() {
